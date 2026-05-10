1d6d5b78fab5cff9a1f0229e1706a2346003f80b
liviacavalcanti <livia.juliao@ccc.ufcg.edu.br>
2026-05-10 12:08:15 +0200

    add find median solution

---
+++ array/find_median.py
@@ -0,0 +1,31 @@
+"""
+The median is the middle value in an ordered integer list. 
+If the size of the list is even, there is no middle value, and the median is the mean of the two middle values.
+
+For example, for arr = [2,3,4], the median is 3.
+For example, for arr = [2,3], the median is (2 + 3) / 2 = 2.5.
+"""
+import heapq
+
+minheap = []
+maxheap = []
+def addNum(num: int) -> None:
+    if len(minheap) == len(maxheap):
+        heapq.heappush(minheap, num)
+        heapq.heappush(maxheap, heapq.heappop(minheap))
+    else:
+        heapq.heappush(maxheap, num)
+        heapq.heappush(minheap, heapq.heappop(maxheap))
+
+
+def findMedian() -> float:
+    n = len(maxheap) + len(minheap)
+    if n % 2 == 1:
+        return maxheap[0]
+    else:
+        return (maxheap[0] + heapq.nlargest(1, minheap)[0]) / 2.0
+    
+for num in [1,2,3,4]:
+    addNum(num)
+    print(minheap, maxheap)
+    print(findMedian())
