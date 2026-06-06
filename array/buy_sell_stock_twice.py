cc2b1dd7fdbbf79a213999678b456f5414d1bd76
livia cavalcanti <livia.juliao@ccc.ufcg.edu.br>
2026-06-06 17:23:16 +0200

    implement the two passes algorithm

---
+++ array/buy_sell_stock_twice.py
@@ -0,0 +1,21 @@
+def buy_sell_twice(prices):
+    min_so_far = float('inf')
+    max_profif = - float('inf')
+
+    forward_profit = []
+    for i in range(len(prices)):
+        min_so_far = min(min_so_far, prices[i])
+        # max_profif = max(max_profif, prices[i] - min_so_far)
+        forward_profit.append(prices[i] - min_so_far)
+        print(forward_profit)
+
+    max_so_far = -float('inf')
+    for i in range(len(prices)-2, -1, -1):
+        max_so_far = max(max_so_far, prices[i])
+        max_profif = max(max_profif, forward_profit[i-1] + (max_so_far-prices[i]))
+    
+    return max_profif
+
+stock_price = [12,11,1.3,9,12,8,14,1,3,15]
+
+print(buy_sell_twice(stock_price))
