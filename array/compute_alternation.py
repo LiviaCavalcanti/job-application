cf53deb043a3ce1c4104b4c3b994357b84caee1e
livia cavalcanti <livia.juliao@ccc.ufcg.edu.br>
2026-06-06 17:57:48 +0200

    simplify the sorted reverse definition

--- array/compute_alternation.py
+++ array/compute_alternation.py
@@ -5,16 +5,10 @@ B having the property that B[0] < B[1] >B [2] < B[3] > [4]< B[5] > ...
 
 def alternation(array):
     for i in range(len(array)):
-        if i % 2: # even
-            array[i: i+2] = sorted(array[i:i+2], reverse=True)
-        else:
-            array[i:i+2] = sorted(array[i:i+2], reverse=False)
-
+        array[i: i+2] = sorted(array[i:i+2], reverse=not (i%2))
+    return array
 
 
 stock_price = [12,11,1.3,9,12,8,14,1,3,15]
 
-alternation(array=stock_price)
-
-# in-place changes
-print(stock_price)
+print(alternation(array=stock_price))
